1c7ea34b0fee23edcb2e92dbdf27e2e253113526
Trisha-Ray <trisha.ray@stu.ocsb.ca>
2025-06-04 13:07:56 -0400

    working on js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,10 @@
 // Created by: Trisha Ray
 // Created on: Feb 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function factorEquation() {
+  let equation = parseFloat(document.getElementById("equation").value)
+  
+}
