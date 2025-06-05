213df758e097c0390139217634a19bf7c60c35b0
Trisha-Ray <trisha.ray@stu.ocsb.ca>
2025-06-05 13:11:58 -0400

    working on js

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # ICD2O-Final-Project
-My program lets the user enter a factoring equation, and the program factors it.
+My program lets the user enter a positive whole number, and the program factors it.
 
 The web application can be viewed at: [Link](https://mths-icd2o-1-2024.github.io/ICD2O-Final-Project-trisha.ray/) 
---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Trisha Ray
+// Created on: Feb 2025
+// This file contains the JS functions for index.html
+"use strict"
+
+// This function factors a number
+function factorNumber() {
+  let inputValue = document.getElementById("number").value
+  let number = parseInt(inputValue)
+  let text = ""
+
+   if (isNaN(number) || number <= 0) {
+    text = "Please enter a positive whole number to factor."
+  } else {
+    let factors = ""
+    let currentNumber = 1
+    while (currentNumber <= number) {
+  }
+ }
+}
